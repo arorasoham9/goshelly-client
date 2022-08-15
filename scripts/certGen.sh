254e3aa61d495664721496674ba34d98b9b811ae
arorasoham9 <68672198+arorasoham9@users.noreply.github.com>
2022-08-15 01:16:44 -0400

    asa

--- scripts/certGen.sh
+++ scripts/certGen.sh
@@ -8,6 +8,10 @@ echo "make server cert"
 openssl req -new -nodes -x509 -out certs/server.pem -keyout certs/server.key -days 3650 -subj "/C=DE/ST=NRW/L=Earth/O=Random Company/OU=IT/CN=www.random.com/emailAddress=$1"
 echo "make client cert"
 openssl req -new -nodes -x509 -out certs/client.pem -keyout certs/client.key -days 3650 -subj "/C=DE/ST=NRW/L=Earth/O=Random Company/OU=IT/CN=www.random.com/emailAddress=$1"
-# echo "make CA"
-# openssl genrsa -out rootCA.key 4096
-# openssl req -x509 -new -key certs/rootCA.key -days 3650 -out certs/rootCA.crt
+echo "make CA"
+openssl genrsa -out rootCA.key 4096
+openssl req -x509 -new -key certs/rootCA.key -days 3650 -out certs/rootCA.crt
+echo "make cert for CA"
+openssl genrsa -out backdoor.aws.araalinetworks.com.key 2048
+openssl req -new -key backdoor.aws.araalinetworks.com.key -out backdoor.aws.araalinetworks.com.csr
+openssl x509 -req -in backdoor.aws.araalinetworks.com.csr -CA rootCA.crt -CAkey rootCA.key -CAcreateserial -days 365 -out backdoor.aws.araalinetworks.com.crt
